5907fc6b2ed5b628c0c6b61b875ed68681e69980
Tomasz Gajewski (JamesBlond) <9467981+tgajewski007@users.noreply.github.com>
2013-12-07 14:32:23 +0000

    + example doReverse  reverse engeenering script

--- ReverseEngenereing/doReverse.php
+++ ReverseEngenereing/doReverse.php
@@ -13,98 +13,40 @@ include "ReverseForeginKey.php";
 include "ReversePrimaryKey.php";
 include "ReverseCreator.php";
 
-// define("ORA_SERVER", "10.32.80.11");
-// define("ORA_PORT", "1521");
-// define("ORA_SID", "ORA11");
-// define("ORA_USERNAME", "EN");
-// define("ORA_PASSWORD", "1");
-// define("ORA_SCHEMA", "EN");
-// include "oracle/DB.php";
-// include "oracle/OracleParams.php";
-// include "oracle/OracleParam.php";
-// include "oracle/OracleProxy.php";
-
 /*
-define("DB_HOST", "localhost");
-define("DB_SCHEMA", "trans");
-define("DB_USER", "root");
-define("DB_PASS", "1");
-include "mysql/DB.php";
-include "mysql/MySQLProxy.php";
-
-$folder = "O:\\wwwroot\\TransSped\\include\\";
-$project = new Project();
-$project->setProjectFolder($folder);
-$project->setErrorPrefix("TS:");
-$project->setDataBaseStyle(DataBaseStyle::MYSQL);
-$project->setAuthor("Tomasz Gajewski");
-$project->setName("EnMarket");
-$project->setXmlFile("O:\\wwwroot\\TransSped\\Doc\\dao.xml");
-
-$x = new MySQLProxy();
-$p = new ReverseCreator($project, $x);
-$p->schemaName = "DB_SCHEMA";
-$p->indexTableSpace = "EN_IND";
-$p->GO();
- */
-
-/*
-define("DB_HOST", "localhost");
-define("DB_SCHEMA", "tomurb02_enmarke");
-define("DB_USER", "root");
-define("DB_PASS", "1");
-include "mysql/DB.php";
-include "mysql/MySQLProxy.php";
-
-$folder = "O:\\wwwroot\\EnMarket\\common\\";
-$project = new Project();
-$project->setProjectFolder($folder);
-$project->setErrorPrefix("EM:");
-$project->setDataBaseStyle(DataBaseStyle::MYSQL);
-$project->setAuthor("Tomasz Gajewski");
-$project->setName("EnMarket");
-$project->setXmlFile("O:\\wwwroot\\EnMarket\\Doc\\dao.xml");
-
-$x = new MySQLProxy();
-$p = new ReverseCreator($project, $x);
-$p->schemaName = "DB_SCHEMA";
-$p->indexTableSpace = "EN_IND";
-$p->GO();
+define("ORA_SERVER", "localhost");
+define("ORA_PORT", "1521");
+define("ORA_SID", "xe");
+define("ORA_USERNAME", "scot");
+define("ORA_PASSWORD", "tiger");
+define("ORA_SCHEMA", "test_database");
+include "oracle/DB.php";
+include "oracle/OracleParams.php";
+include "oracle/OracleParam.php";
+include "oracle/OracleProxy.php";
 */
+
+
 define("DB_HOST", "localhost");
-define("DB_SCHEMA", "parkmar");
+define("DB_SCHEMA", "test_database");
 define("DB_USER", "root");
 define("DB_PASS", "1");
 include "mysql/DB.php";
 include "mysql/MySQLProxy.php";
 
-$folder = "O:/wwwroot/ParkMar/include/";
+$folder = "O:\\wwwroot\\TestApp\\common\\";
 $project = new Project();
 $project->setProjectFolder($folder);
-$project->setErrorPrefix("PM:");
+$project->setErrorPrefix("TEST:");
 $project->setDataBaseStyle(DataBaseStyle::MYSQL);
 $project->setAuthor("Tomasz Gajewski");
-$project->setName("ParkMar");
-$project->setXmlFile("O:/wwwroot/ParkMar/Doc/dao.xml");
+$project->setName("TestApp");
+$project->setXmlFile("O:\\wwwroot\\TestApp\\Doc\\dao.xml");
 
 $x = new MySQLProxy();
 $p = new ReverseCreator($project, $x);
 $p->schemaName = "DB_SCHEMA";
-$p->indexTableSpace = "EN_IND";
 $p->GO();
-// $folder = "O:\\wwwroot\\TransSped\\include\\";
-// $project = new Project();
-// $project->setProjectFolder($folder);
-// $project->setErrorPrefix("TS:");
-// $project->setDataBaseStyle(DataBaseStyle::MYSQL);
-// $project->setAuthor("Tomasz Gajewski");
-// $project->setName("TransSped");
-// $project->setXmlFile("O:\\wwwroot\\TransSped\\Doc\\dao.xml");
 
-// $x = new MySQLProxy();
-// $p = new ReverseCreator($project, $x);
-// $p->schemaName = "DB_SCHEMA";
-// $p->indexTableSpace = "EN_IND";
-// $p->GO();
 
 ?>
