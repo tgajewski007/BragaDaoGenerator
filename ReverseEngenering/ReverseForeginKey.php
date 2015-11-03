af4ec831aae3dea72c849cdbdf1581fe0331bcab
Tomasz Gajewski (JamesBlond) <9467981+tgajewski007@users.noreply.github.com>
2015-11-03 18:14:46 +0000

    revert

---
+++ ReverseEngenering/ReverseForeginKey.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created on 07-04-2013 13:46:12
+ * author Tomasz Gajewski
+ * package frontoffice
+ * error prefix
+ */
+class ReverseForeginKey
+{
+	// -------------------------------------------------------------------------
+	public $refTableName = null;
+	public $refTableSchema = null;
+	public $columns = array();
+	// -------------------------------------------------------------------------
+}
+?>
