55589e9f78324fa09345fef5097c806838ee0bd1
Tomasz Gajewski <tgajewski007@gmail.com>
2023-01-26 18:20:48 +0100

    cleanup

--- src/generator/Project.php
+++ src/generator/Project.php
@@ -9,7 +9,7 @@ namespace braga\daogenerator\generator;
 class Project
 {
 	// -------------------------------------------------------------------------
-	const VERSION = "4.0.11";
+	const VERSION = "4.0.12";
 	// -------------------------------------------------------------------------
 	protected $xmlFile = null;
 	protected $projectFolder = ".";
